bd62e8ac4fad3b3c4b80c6ef038b15ed459bc732
julian-e-herrera <je.herrera@globant.com>
2020-09-17 17:52:47 -0300

    test crear elemetos dinamicos

--- js/builder.js
+++ js/builder.js
@@ -1,49 +1,71 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const crudder = dominio => recurso => {
+        const url = `${dominio}/${recurso}`
+        return ({
+            create: (x) => fetch(url, {
+                method: 'POST',
+                body: JSON.stringify(x),
+            }).then(x => x.json()),
+            get: () => fetch(url).then(x => x.json())
+                .catch(e => console.log(e))
+        })
+    }
 
 
-document.addEventListener('DOMContentLoaded',() => {
-const crudder = dominio => recurso => {
-    const url = `${dominio}/${recurso}`
-    return ({
-        create: (x) => fetch(url, {
-            method: 'POST',
-            body: JSON.stringify(x),
-        }).then(x => x.json()),
-        get: () => fetch(url).then(x => x.json())
-            .catch(e => console.log(e))
-    })
-}
+    let Base = crudder('https://jsonplaceholder.typicode.com');
+    let Todos = Base('photos');
 
-
-let Base = crudder('https://jsonplaceholder.typicode.com');
-let Todos = Base('photos');  
-
-let bases= fetch('https://jsonplaceholder.typicode.com/photos').then(x => x.json()).then(x =>{
-    let element;    
-        for ( element of x){
-            if (element.id < 4) {//esto podria marcar un limite de muestras o modificarlo desde un evento
+    let photos = Todos.get().then(x => {
+        let element;
+        for (element of x) {
+            if (element.id < 4) { //esto podria marcar un limite de muestras o modificarlo desde un evento
                 element = {
                     'title': element.title,
                     'url': element.url
                 }
-            let box = document.querySelector(".box");
-            let card = document.createElement("div");
-            card.setAttribute('class',element.title);
-            console.log(x.title);// nt found 
-            let spantitle = document.createElement("span");
-            spantitle.setAttribute('class','span-title');
-            spantitle.innerHTML=element.title;
-            card.appendChild(spantitle);
-            let photo = document.createElement("img");
-            photo.setAttribute('class','fotito');
-            photo.setAttribute('loading','lazy');
-            card.appendChild(photo);
-            box.appendChild(card);
-            photo.setAttribute("src",element.url);
+              //  createCard(element);
             }
-// })
-        console.log(element);
-    };
-    return element;
-}) 
+        };
+        return element;
+    })
 
+
+    let createCard = (el) => {
+        let box = document.querySelector(".box");
+        let card = document.createElement("div");
+        card.setAttribute('class', el.title);
+        let spantitle = document.createElement("span");
+        spantitle.setAttribute('class', 'span-title');
+        spantitle.innerHTML = el.title;
+        card.appendChild(spantitle);
+        let photo = document.createElement("img");
+        photo.setAttribute('class', 'fotito');
+        photo.setAttribute('loading', 'lazy');
+        card.appendChild(photo);
+        box.appendChild(card);
+        photo.setAttribute("src", el.url);
+    }
+
+    
+    let createForm = () => {
+        let box = document.querySelector(".box");
+        let form = document.createElement("form");
+        form.setAttribute('class', 'formulario');
+        let input_user = document.createElement("input");
+        input_user.setAttribute('class', 'input_user');
+        input_user.setAttribute('placeholder', 'user');
+        let input_pass = document.createElement("input");
+        input_pass.setAttribute('class', 'input_pass');
+        input_pass.setAttribute('placeholder', 'pass');
+        input_pass.setAttribute('type', 'password');
+        form.appendChild(input_user);
+        form.appendChild(input_pass);
+        let button = document.createElement("button");
+        button.setAttribute('class', 'btn search');
+        button.innerHTML="Login";
+        form.appendChild(button);
+        box.appendChild(form);
+    }
+
+    createForm();
 });
